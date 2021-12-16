71ae3f4e7e4465d5a47d4517da4698b7ffd8ba41
LukaZ <luka.zubcic@spica.com>
2021-12-16 21:52:39 +0100

    a

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int counter = 1;
+
+    /*for (counter = 1; counter < 10; counter = counter + 2)
+    {
+        std::cout << "Vrijednost: " << counter;
+        std::cout << "\n";
+    }*/
+
+
+    while (counter < 10)
+    {
+        std::cout << "Vrijednost: " << counter << "\n";
+        counter++;
+    }
+
+    std::cout << "\n\n";
+    std::cout << "STOP";
+
+}
